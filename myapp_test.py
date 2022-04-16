c32331ab24cfa036a498190c460d064428f167e4
Alexis Rodriguez <alexisrodgtz@gmail.com>
2022-04-16 14:02:27 -0500

    Unit test fix to remove warning

--- myapp_test.py
+++ myapp_test.py
@@ -4,7 +4,7 @@ import myapp
 class TestMyApp(unittest.TestCase):
 
     def test_hi(self):
-      self.assertEquals(myapp.say_hi(),'Hola soy Alexis')
+      self.assertEqual(myapp.say_hi(),'Hola soy Alexis')
 
 if __name__=='__main__':
   unittest.main()
