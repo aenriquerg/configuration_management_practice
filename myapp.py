91da1f22a4f16050b39febfcb5a88ecba438ccbd
Alexis Rodriguez <alexisrodgtz@gmail.com>
2022-05-11 17:30:15 -0500

    testing cd

--- myapp.py
+++ myapp.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/hi")
 def say_hi():
     """Return a greeting for Alexis"""
-    return 'Hola soy Alexis'
+    return 'Hola soy Alexis '
 
 if __name__=='__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
